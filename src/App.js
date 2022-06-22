59ebde941b5de8e8100fcca3c1272fc2ee56f797
Maianne Thornton <maianne.thornton@gmail.com>
2022-06-22 14:47:48 -0400

    feat: added container class for styling

--- src/App.js
+++ src/App.js
@@ -36,7 +36,9 @@ function App() {
   }
 
   return (
-    <FlashcardList flashcards={flashcards} />
+    < div className="container">
+      <FlashcardList flashcards={flashcards} />
+    </div >
   )
 }
 const SAMPLE_FLASHCARDS = [
