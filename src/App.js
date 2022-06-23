5cbe7c1ab1f6de71f6c180b260d5891b8d7a7b94
Maianne Thornton <maianne.thornton@gmail.com>
2022-06-23 14:49:04 -0400

    "feat: added form functionality, removed sample cards"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import FlashcardList from './FlashcardList';
 import axios from 'axios'
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
 
   useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php')
+      .then(res => {
+        setCategories(res.data.trivia_categories)
+      })
+  }, [])
+
+  // converts HTML erroneously displaying to a string
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    // prevents form from submitting without going through our React code
+    e.preventDefault()
     axios
-      .get('https://opentdb.com/api.php?amount=10')
+      .get('https://opentdb.com/api.php', {
+        // Allows us to chose our own parameters
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
+        }
+      })
       .then(res => {
         setFlashcards(res.data.results.map((questionItem, index) => {
           const answer = decodeString(questionItem.correct_answer)
@@ -25,46 +51,39 @@ function App() {
             options: options.sort(() => Math.random() - .5),
           }
         }))
-        console.log(res.data);
       })
-  }, [])
-
-  // converts HTML erroneously displaying to a string
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
   }
 
   return (
-    < div className="container">
-      <FlashcardList flashcards={flashcards} />
-    </div >
+    // wrapping the form in a fragment
+    <>
+      <form className='header' onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          {/* populating the category drop down list */}
+          <select id="category" ref={categoryEl}>
+            {categories.map(category => {
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Number Of Questions</label>
+          <input type="number" id="amount"
+            // step is set to 1 to only allow whole numbers
+            min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+        <div className="form-group">
+          <button className='btn'>Generate</button>
+        </div>
+
+      </form>
+
+      < div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div >
+    </>
   )
 }
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: 'What is 2 + 2?',
-    answer: '4',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  },
 
-  {
-    id: 2,
-    question: 'Question 2?',
-    answer: 'Answer',
-    options: [
-      'Answer',
-      'Answer 1',
-      'Answer 2',
-      'Answer 3'
-    ]
-  },
-]
 export default App;
